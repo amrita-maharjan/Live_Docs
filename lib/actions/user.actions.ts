4a8776c59730bc4a2477801f33d0dee40589a62a
amrita <maharjanamrita94@gmail.com>
2026-01-20 18:16:45 +0545

    feat: project completion

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { clerkClient } from "@clerk/nextjs/server";
+import { parseStringify } from "../utils";
+import { liveblocks } from "../liveblocks";
+
+export const getClerksUser = async ({ userIds }: { userIds: string[] }) => {
+  try {
+    const client = await clerkClient();
+    const { data } = await client.users.getUserList({
+      emailAddress: userIds,
+    });
+    const users = data.map((user: any) => ({
+      id: user.id,
+      name: `${user.firstName}${user.lastName}`,
+      email: user.emailAddresses[0].emailAddress,
+      avatar: user.imageUrl,
+    }));
+    const sortedUsers = userIds.map((email) =>
+      users.find((user: any) => user.email === email)
+    );
+    return parseStringify(sortedUsers);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getDocumentsUser = async ({
+  roomId,
+  currentUser,
+  text,
+}: {
+  roomId: string;
+  currentUser: string;
+  text: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+    const users = Object.keys(room.usersAccesses).filter(
+      (email) => email !== currentUser
+    );
+    if (text.length) {
+      const lowerCaseText = text.toLowerCase();
+      const filteredusers = users.filter((email: string) =>
+        email.toLowerCase().includes(lowerCaseText)
+      );
+      return parseStringify(filteredusers);
+    }
+    return parseStringify(users);
+  } catch (error) {
+    console.log(`Error fetching document user:${error}`);
+  }
+};
